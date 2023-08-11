564e9a60d40b22971c6e959e897a6e579f4497da
burtovoy <volgobur@gmail.com>
2023-08-11 12:21:37 +0300

    fix

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 const html = fs.readFileSync('public/main.html', 'utf8');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => res.type('html').send(html));
 
 app.listen(port, () => {
